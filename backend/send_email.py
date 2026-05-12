5056e553e480623806b753e0c09d392bea0286ce
Brandon Del Mundo <brandon.delmundo@gmail.com>
2026-05-11 22:27:34 -0700

    quick

--- backend/send_email.py
+++ backend/send_email.py
@@ -55,7 +55,12 @@ def send_email(sender, password, recipients, subject, body_text, body_html):
         smtp_server = 'smtp.gmail.com'  # default fallback
         port = 465
 
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.starttls()
-        server.login(sender, password)
-        server.sendmail(sender, recipients, msg.as_string())
+    if port == 465:
+        with smtplib.SMTP_SSL(smtp_server, port) as server:
+            server.login(sender, password)
+            server.sendmail(sender, recipients, msg.as_string())
+    else:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls()
+            server.login(sender, password)
+            server.sendmail(sender, recipients, msg.as_string())
