a8852a46aad2aa6854ca3269046de38ab37aa613
Brandon Del Mundo <brandon.delmundo@gmail.com>
2026-05-11 22:26:31 -0700

    Test

--- backend/send_email.py
+++ backend/send_email.py
@@ -47,13 +47,13 @@ def send_email(sender, password, recipients, subject, body_text, body_html):
     msg.attach(MIMEText(body_html, 'html'))
     if 'gmail' in sender:
         smtp_server = 'smtp.gmail.com'
-        port = 587
+        port = 465
     elif 'outlook' in sender or 'hotmail' in sender or 'live' in sender:
         smtp_server = 'smtp.office365.com'
         port = 587
     else:
         smtp_server = 'smtp.gmail.com'  # default fallback
-        port = 587
+        port = 465
 
     with smtplib.SMTP(smtp_server, port) as server:
         server.starttls()
